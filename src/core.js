be0d60023996cf7a3bbeb6b6ae383024d9063d88
dearshrewdwit <dearshrewdwit@gmail.com>
2022-09-15 11:09:02 +0100

    disable no-undef

--- src/core.js
+++ src/core.js
@@ -1,22 +1,16 @@
 // 1. Create a function named bakingTime that returns 50
 
-
-
 // 2. Create a function named remainingBakeTime that accepts one parameter:
 // - the number of minutes your cake has been in the oven.
 //
 // It must return how many minutes your cake has left based on your parameter
 // and the result of the bakingTime function
 
-
-
 // 3. Create a function named calculatePreparationTime that accepts one parameter:
 // - the number of layers your cake has
 //
 // It must return how many minutes it will take to prepare your cake, based on
 // each layer taking 3 minutes to prepare
-
-
 
 // 4. Create a function named totalTimeSpent that accepts two parameters:
 // - the number of layers your cake has
@@ -25,13 +19,14 @@
 // which is the sum of the preparation time and the number of minutes it's been in the oven.
 // Use your calculatePreparationTime function in the calculation.
 
-
-
-
 // Don't change the code below this line
 module.exports = {
-    bakingTime,
-    remainingBakeTime,
-    calculatePreparationTime,
-    totalTimeSpent
+  /* eslint-disable no-undef */
+  bakingTime,
+  /* eslint-disable no-undef */
+  remainingBakeTime,
+  /* eslint-disable no-undef */
+  calculatePreparationTime,
+  /* eslint-disable no-undef */
+  totalTimeSpent
 }
